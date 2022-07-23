cf13a86a4c1312b2f78afda05e213dc0ee26e143
yossu-y <yangyuub@gmail.com>
2022-07-23 14:35:01 +0000

    [update]fizz_buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -10,10 +10,13 @@ def fizz_buzz(number)
   end
 end
 
+# 決まった数字まででfizz_buzzを表示
 numbers = 1..30
 numbers.each do |number|
   puts fizz_buzz(number)
 end
+
+# 入力した数字を判断
 
 # puts "数字を入れてください"
 # input = gets.to_i
