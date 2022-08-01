754e32d4233b8c6c3ce6f31b4ee993fca445d073
yossu-y <yangyuub@gmail.com>
2022-08-01 11:54:44 +0000

    update

--- multiplication.rb
+++ multiplication.rb
@@ -1,16 +1,9 @@
-x_line = 9
-y_line = 9
-size = (x_line * y_line).to_s.size
-
-puts ("+" + "-" * size) * x_line + "+"
-
-# Y line is output
-(1..y_line).each do |y|
-
-    # X line is output
-    (1..x_line).each do |x|
-        puts "|%#{size}d" % (x * y)
+for i in 1..9 do
+  for j in 1..9 do
+    if i * j < 10 then
+     print " "
     end
-    puts "\n" + ("+" + "-" * size) * x_line
-
+    print i * j, " "
+  end
+  puts
 end
